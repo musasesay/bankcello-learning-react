183f80ae93b2b36b676d7c099bdd423a6ce3c4da
John D. Aynedjian <johndavid5@yahoo.com>
2018-05-08 23:05:16 -0400

    redux: reducers...

--- chapter-08/color-organizer-redux/src/store/reducers.js
+++ chapter-08/color-organizer-redux/src/store/reducers.js
@@ -1,5 +1,15 @@
 import C from '../constants'
 
+/** The color reducer is a function that creates a new
+* object or rates an existing one.
+*
+* You'll notice that the RATE_COLOR action passes an ID
+* that's not used by the color reducer.
+*
+* That's because the ID of this action is used to location
+* the color in an entirely different reducer.  One action
+* object can impact several reducers.
+*/
 export const color = (state = {}, action) => {
     switch (action.type) {
         case C.ADD_COLOR:
@@ -19,6 +29,10 @@ export const color = (state = {}, action) => {
             //  Object.assign({rating:action.rating}, state)
             // -- to assign the value of the current state
             // to a new object.
+			//
+			// Checks that the id's match before replacing,
+			// thus can be used in a shotgun approach
+			// via map() in colors reducer... 
             return (state.id !== action.id) ?
                 state :
                 {
@@ -34,18 +48,49 @@ export const color = (state = {}, action) => {
     }
 }
 
+/* The color reducer is designed to manage leaves on 
+* the colors branch in our state tree.
+*
+* The colors reducer will be used to manage the entire
+* colors branch.
+*
+* The colors reducer is concerned with the array
+* of colors.  It uses the color reducer to focus
+* on the individual color objects.
+*/
 export const colors = (state = [], action) => {
     switch (action.type) {
         case C.ADD_COLOR :
+			//   Creates a new array by concatenating all of the
+			// values of the existing state array with a 
+			// new color object.
+			//
+			//   The new color is created
+			// by passing a blank state object and the action
+			// to the color reducer.
             return [
                 ...state,
                 color({}, action)
             ]
         case C.RATE_COLOR :
+			//   Returns a new array of colors with the desired
+			// color rated.
+			//   The colors reducer locates the
+			// color to be rated within the current state
+			// array.
+			//   It then uses the color reducer to obtain
+			// the newly rated color object and replaces it in
+			// the array.
+			//   Actually, it calls map() on every item
+			// in the state array, and lets the color reducer
+			// figure out if the ids match, and if so,
+			// it replaces with an updated color object.
             return state.map(
                 c => color(c, action)
             )
         case C.REMOVE_COLOR :
+			// Creates a new array by filtering out the
+			// desired color to remove.
             return state.filter(
                 c => c.id !== action.id
             )
