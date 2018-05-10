b2a32de89bf28798979325a8c22d80933b9f49b2
John D. Aynedjian <johndavid5@yahoo.com>
2018-05-09 22:00:58 -0400

    action creators...

--- chapter-08/color-organizer-redux/src/store/index.js
+++ chapter-08/color-organizer-redux/src/store/index.js
@@ -13,6 +13,25 @@ const logger = store => next => action => {
     return result
 }
 
+// English Translation:
+//
+// (It's a function that returns a function that returns a function...)
+//
+// var logger = function logger(store) {
+//     return function (next) {
+//         return function (action) {
+//             var result = void 0;
+//             console.groupCollapsed("dispatching", action.type);
+//             console.log('prev state', store.getState());
+//             console.log('action', action);
+//             result = next(action);
+//             console.log('next state', store.getState());
+//             console.groupEnd();
+//             return result;
+//         };
+//     };
+// };
+
 const saver = store => next => action => {
     let result = next(action)
     localStorage['redux-store'] = JSON.stringify(store.getState())
