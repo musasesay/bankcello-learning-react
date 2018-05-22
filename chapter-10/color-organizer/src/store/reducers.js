712f996fe87a14acd34aadd75bcb0f5c5b34ed2e
John D. Aynedjian <johndavid5@yahoo.com>
2018-05-22 14:44:10 -0400

    Minimum to pass, but allowing state to be mutated... (Pure functions should not mutate input)

--- chapter-10/color-organizer/src/store/reducers.js
+++ chapter-10/color-organizer/src/store/reducers.js
@@ -1,5 +1,19 @@
 import C from '../constants'
 
 export const color = (state = {}, action={ type: null }) => {
-    return state
+    switch (action.type) {
+        case C.ADD_COLOR:
+            return {
+                id: action.id,
+                title: action.title,
+                color: action.color,
+                timestamp: action.timestamp,
+                rating: 0
+            }
+        case C.RATE_COLOR:
+            state.rating = action.rating
+            return state     
+        default :
+            return state
+    }
 }
