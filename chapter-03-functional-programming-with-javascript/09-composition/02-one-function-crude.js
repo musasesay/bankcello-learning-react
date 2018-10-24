d773d9fbb13de9e9ebc698c0e4d08c7886dcef32
John Aynedjian <john.david.aynedjian@gmail.com>
2018-10-23 20:10:04 -0400

    comments...

--- chapter-03-functional-programming-with-javascript/09-composition/02-one-function-crude.js
+++ chapter-03-functional-programming-with-javascript/09-composition/02-one-function-crude.js
@@ -2,6 +2,11 @@
 const { createClockTime, appendAMPM, civilianHours, removeDate } = require('./timeFuncs'); 
 
 // Not the best way to compose all of these into one function
+// But then again, if you look at https://redux.js.org/api/compose,
+// they say...
+//   "All compose does is let you write deeply nested function
+//   transformations without the rightward drift of the code.
+//   Don't give it too much credit!"
 const oneFunction = date =>
 	removeDate(
           civilianHours(
@@ -11,4 +16,4 @@ const oneFunction = date =>
           )
    	)
 
-console.log(`SHEMP: Moe, oneFunction(new Date()) = `, oneFunction(new Date()))
+console.log(`SHEMP: Moe, widhout usin' compose(), oneFunction(new Date()) = `, oneFunction(new Date()))
